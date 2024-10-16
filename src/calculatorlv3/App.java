4b2067e6cb83b605c021ce5959548c6f06c58fd2
KM4HS <ac4hsu@gmail.com>
2024-10-16 17:15:11 +0900

    Feat: Add result filter 계산 결과 출력에 입력값보다 더 큰 값만 출력하는 필터 적용

--- src/calculatorlv3/App.java
+++ src/calculatorlv3/App.java
@@ -1,13 +1,12 @@
 package calculatorlv3;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Parser parser = new Parser();
-        ArrayList<CalculationHistory> calculationHistories = new ArrayList<>();
+        CalculationHistory calculationHistory = new CalculationHistory();
 
         while (true) {
             ArithmeticCalculator calculator;
@@ -16,7 +15,7 @@ public class App {
             double num2 = 0;
             char operator=' ';
 
-            System.out.print("첫 번째 숫자를 입력하세요:");
+            System.out.print("첫 번째 숫자를 입력하세요: ");
             try{
                 num1 = parser.parseNumber(sc.nextLine());
             }catch(WrongInputException e){
@@ -25,7 +24,7 @@ public class App {
                 continue;
             }
 
-            System.out.print("두 번째 숫자를 입력하세요:");
+            System.out.print("두 번째 숫자를 입력하세요: ");
             try{
                 num2 = parser.parseNumber(sc.nextLine());
             }catch(WrongInputException e){
@@ -72,29 +71,39 @@ public class App {
                 calculator = new ArithmeticCalculator<>(operator, num1, num2);
             }
             result = calculator.calculate();
-            calculationHistories.add(new CalculationHistory(num1, num2, operator, result));
+            calculationHistory.addHistories(num1, num2, operator, result);
             if(isInteger) {
                 System.out.println("결과 : "+(int)result);
             }else {
                 System.out.println("결과 : "+result);
             }
 
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
             if (sc.nextLine().equals("exit")) {
                 break;
             }
 
-            if (!calculationHistories.isEmpty()) {
-                System.out.println("가장 먼저 저장된 데이터를 삭제하시겠습니까? (remove 입력 시 삭제)");
-                if (sc.nextLine().equals("remove")) {
-                    calculationHistories.remove(0);
-                    System.out.println("가장 먼저 저장된 데이터가 삭제되었습니다.");
-                }
+            if (calculationHistory.isHistoriesEmpty()) {
+                continue;
+            }
 
+            System.out.print("가장 먼저 저장된 데이터를 삭제하시겠습니까? (remove 입력 시 삭제): ");
+            if (sc.nextLine().equals("remove")) {
+                calculationHistory.removeFirstHistory();
+                System.out.println("가장 먼저 저장된 데이터가 삭제되었습니다.");
+            }
+
+            System.out.print("결과값을 필터링해서 보시겠습니까? (yes 입력 시 필터 적용): ");
+            boolean flag = sc.nextLine().equals("yes");
+            if(flag){
+                System.out.print("기준 숫자를 입력해주세요 : ");
+                double num = Double.parseDouble(sc.nextLine());
+                System.out.println("지금까지 저장된 " + num + " 이상의 데이터 : ");
+                calculationHistory.getCalculationHistoriesAsString(num);
+
+            }else{
                 System.out.println("지금까지 저장된 데이터 : ");
-                for (CalculationHistory history : calculationHistories) {
-                    System.out.println(history.getCalculationHistoryAsString());
-                }
+                calculationHistory.getCalculationHistoriesAsString();
             }
             System.out.println();
         }
--- src/calculatorlv3/CalculationHistory.java
+++ src/calculatorlv3/CalculationHistory.java
@@ -1,19 +1,51 @@
 package calculatorlv3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalculationHistory {
     double number1;
     double number2;
     char operator;
     double result;
 
-    CalculationHistory(double number1, double number2, char operator, double result){
+    ArrayList<CalculationHistory> calculationHistories;
+
+    CalculationHistory(){
+        this.calculationHistories = new ArrayList<>();
+    }
+    private CalculationHistory(double number1, double number2, char operator, double result){
         this.number1 = number1;
         this.number2 = number2;
         this.operator = operator;
         this.result = result;
     }
 
-    public String getCalculationHistoryAsString(){
-        return number1 + " " + operator + " " + number2 + " = " + result;
+    public boolean isHistoriesEmpty(){
+        return calculationHistories.isEmpty();
+    }
+    public void addHistories(double number1, double number2, char operator, double result){
+        calculationHistories.add(new CalculationHistory(number1, number2, operator, result));
+    }
+    public void removeFirstHistory(){
+        calculationHistories.remove(0);
+    }
+
+    public void getCalculationHistoriesAsString(){
+        for(CalculationHistory c : calculationHistories){
+            System.out.println(c.number1 + " " + c.operator + " " + c.number2 + " = " + c.result);
+        }
+    }
+    public void getCalculationHistoriesAsString(double num){
+        List<CalculationHistory> temp = calculationHistories.stream()
+                .filter(ch -> ch.getResult() > num)
+                .toList();
+        for(CalculationHistory t : temp){
+            System.out.println(t.number1 + " " + t.operator + " " + t.number2 + " = " + t.result);
+        }
+    }
+
+    private double getResult(){
+        return this.result;
     }
 }
